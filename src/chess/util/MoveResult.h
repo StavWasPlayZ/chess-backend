913c92d18e8ed21276b36be6fbf623c4bb6ab1e4
StavWasPlayZ <stavsolomon101@gmail.com>
2024-12-15 23:43:39 +0200

    Remove MoveResult::devouredPiece
    The only devoured piece should be the one we will step on.

--- src/chess/util/MoveResult.h
+++ src/chess/util/MoveResult.h
@@ -6,8 +6,9 @@ namespace chess
 {
     struct MoveResult
     {
-        MoveResult(const int statusCode, const Piece* const devouredPiece);
+        // MoveResult(const int statusCode, const Piece* const devouredPiece);
+        MoveResult(const int statusCode);
         const int statusCode;
-        const Piece* const devouredPiece;
+        // const Piece* const devouredPiece;
     };
 }
