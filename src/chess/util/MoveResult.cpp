65341a5254b6a313938050c98c83a4baf92c33fa
StavWasPlayZ <stavsolomon101@gmail.com>
2024-12-16 15:12:07 +0200

    Implement MoveResult

---
+++ src/chess/util/MoveResult.cpp
@@ -0,0 +1,24 @@
+#include "MoveResult.h"
+
+using namespace chess;
+
+#define LEGAL_RESULTS_LEN 3
+
+static constexpr chess::MoveResult LEGAL_RESULTS[LEGAL_RESULTS_LEN] = {
+    MoveResult::VALID,
+    MoveResult::CHECK,
+    MoveResult::CHECKMATE
+};
+
+bool chess::isLegal(const MoveResult moveResult)
+{
+    for (size_t i = 0; i < LEGAL_RESULTS_LEN; i++)
+    {
+        if (moveResult == LEGAL_RESULTS[i])
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
