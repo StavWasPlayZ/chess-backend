2aadb416da25fef503c89fc3eff9df2c116d563e
StavWasPlayZ <stavsolomon101@gmail.com>
2024-12-19 13:02:15 +0200

    Implement King

--- src/chess/piece/King.cpp
+++ src/chess/piece/King.cpp
@@ -1,12 +1,12 @@
-#include "Knight.h"
+#include "King.h"
 
 using namespace chess;
 
-chess::Knight::Knight(Board &board, const Point &position, Player &player) :
+chess::King::King(Board &board, const Point &position, Player &player) :
     Piece(board, position, player)
 {}
 
-MoveResult chess::Knight::validateMove(const Point &destination) const
+MoveResult chess::King::validateMove(const Point &destination) const
 {
     {
         const MoveResult moveRes = Piece::validateMove(destination);
@@ -17,21 +17,18 @@ MoveResult chess::Knight::validateMove(const Point &destination) const
     const Point displacement = getPosition()->displacementFrom(destination);
     const int len1 = abs(displacement.x), len2 = abs(displacement.y);
 
-    if (
-           ((len1 == 1) && (len2 == 2))
-        || ((len1 == 2) && (len2 == 1))
-    )
+    if ((len1 <= 1) && (len2 <= 1))
         return MoveResult::LEGAL_MOVE;
 
     return MoveResult::ILLEGAL_MOVE;
 }
 
-char chess::Knight::marker() const
+char chess::King::marker() const
 {
     return asPlayerMarker('q');
 }
 
-int chess::Knight::value() const
+int chess::King::value() const
 {
     return 9;
 }
